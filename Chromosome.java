76c72e78f7f726472d7b56be9712dfcd3ecbdce5
Sam <49539910+KingShampoo@users.noreply.github.com>
2021-04-11 21:39:04 -0400

    Add files via upload

---
+++ Chromosome.java
@@ -0,0 +1,117 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Chromosome extends ArrayList<Item> implements Comparable<Chromosome>{//chromosome IS an arraylist of items
+    private ArrayList<Item> items = new ArrayList<>();
+    //private static long dummy = 0;
+
+    private int rng(){
+        Random rand = new Random();
+        int rng = rand.nextInt(10);
+        return rng;
+    }
+    public Chromosome(){}
+    public Chromosome(ArrayList<Item> items){
+
+        for (int i = 0; i < items.size(); i++) {
+            if (rng() > 5)
+                items.get(i).setIncluded(true);
+            items.get(i).setIncluded(false);
+        }
+        //add list of items to arraylist
+        //for each item in list 50%chance to trigger included
+        this.items = items;
+        mutate();
+    }
+
+    //takes an array list and turns it into a chromosome
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public Chromosome crossover(Chromosome parentA,Chromosome parentB){
+        Chromosome returnChromosome = new Chromosome();
+
+        for (int i = 0; i < items.size(); i++) {
+            if (rng()>5)
+                returnChromosome.add(parentA.get(i));
+            else
+            returnChromosome.add(parentB.get(i));
+        }
+        //creates a child from 2 chromosomes
+        // 2 chromosomes are this.chromosome and other
+        //use if rng>5 chromosome other gene is passed to child, else this chromosome gene is passed
+        return returnChromosome;
+    }
+
+    public void mutate(){
+        Item token;
+        for (int i = 0; i < items.size(); i++) {
+            if (rng()<=1){
+            token = items.get(i);
+            if (token.isIncluded()==true)
+                token.setIncluded(false);
+            }
+        }
+    }
+
+    public int getFitness(){
+        int weight = 0;
+        int value = 0;
+        int fitness;
+
+     //   dummy = 0;
+      //  for (int i=0; i<this.size()*1000; i++) {
+      //      dummy += i;
+      //  }
+
+        for (int i = 0; i < items.size(); i++) {
+            weight += items.get(i).getWeight();
+        }
+        for (int i = 0; i < items.size(); i++) {
+            value += items.get(i).getValue();
+        }
+
+        if (weight>10) {
+            fitness = 0;
+        } else {
+            fitness = value;
+        }
+        return fitness;
+    }
+
+    public int compareTo(Chromosome other){
+        int returnValue  = 0;
+        if (this.getFitness()> other.getFitness())
+            returnValue = -1;
+        if (this.getFitness()< other.getFitness())
+            returnValue = 1;
+
+        return returnValue;// return -1 if chromosome A is better than chromosome B, or +1 for the opposite, else 0 for equal chromosomes.
+    }
+    @Override
+    public Item get(int i){
+        return items.get(i);
+    }
+    @Override
+    public String toString(){
+        String printStatement = "The items in this chromosome are: ";
+        for (int i = 0; i < items.size(); i++) {
+            Item itemToPrint = items.get(i);
+            printStatement += itemToPrint.toString();
+        }
+         return printStatement;
+        }
+    }
+
+
+
+
+
+    //chromosome represents a list of the 7 items we COULD take with us
+    //each item being shown in the chromosome as a T or F , T being were including it in that chromosome answer.
+    //crossbreading works like this, each gene has a 50% to swap from a selection of 2 chromosomes
+    //{1,2,3,4,5,6,7} use rand.next bool to get a 5050 for each gene
---
+++ GeneticAlgorithm.java
@@ -0,0 +1,94 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class GeneticAlgorithm {
+        public static ArrayList<Item> readData(String fileName) throws FileNotFoundException {//reads data file and uses tokens to build an obj (item)
+            ArrayList<Item> itemList = new ArrayList<>();
+            final String path = fileName;
+            String itemToken;
+            double weightToken;
+            int valueToken;
+
+            try {
+                Scanner inFile1 = new Scanner(new File(path));
+                inFile1.useDelimiter(",|\n");//separates tokens by commas and enter keys
+                while (inFile1.hasNext()) {
+                    itemToken = inFile1.next().trim();
+                    weightToken = Double.parseDouble(inFile1.next().trim());//no white space allowed!
+                    valueToken = Integer.parseInt(inFile1.next().trim());
+                Item newItem = new Item(itemToken,weightToken,valueToken);
+                itemList.add(newItem);//creates item from the tokens and then adds to a list to then make a total item list
+
+                }
+            }
+            catch(FileNotFoundException e){
+                e.printStackTrace();
+            }
+            return itemList;
+        }
+
+    public static ArrayList<Chromosome> initializePopulation( ArrayList<Item> items, int populationSize) {//sets up all the chromosomes with randomly selected T's and F's
+        ArrayList<Chromosome> listToPass = new ArrayList<>();
+
+        for (int i = 0; i < populationSize; i++) {
+            listToPass.add(new Chromosome(items));//generates first 10 chromosomes
+        }
+        return listToPass;
+    }
+
+    public static Collection<Chromosome> sortingMethod (ArrayList<Chromosome> nextGeneration){
+            Collection<Chromosome> returnCollection = new ArrayList<>();
+            Collections.sort(nextGeneration, (o1, o2) -> {
+                int returnvalue = o1.getFitness();
+                if (o2.getFitness()> o1.getFitness())//return highest fitness when comparing two
+                    returnvalue = o2.getFitness();
+
+                return returnvalue;
+            });
+            return returnCollection;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        final String fileName = "C://vm//more_items.txt";
+        final int populationSize = 10;
+        final int numberOfEPOCHS = 1000;
+
+        ArrayList<Item> itemList = new ArrayList(readData(fileName));
+
+        ArrayList<Chromosome> currentGeneration = initializePopulation(itemList,populationSize); // makes a starting population of int populationSize// start with 10 chromosomes
+        ArrayList<Chromosome> nextGeneration = new ArrayList<>();
+
+        for (int i = 0; i < numberOfEPOCHS; i++) {//iterate how many times
+
+            for (int j = 0; j < currentGeneration.size(); j++)
+                nextGeneration.add(currentGeneration.get(j));
+            //step 3 create 5 children using crossover, add to next gen population pool
+
+            for (int j = 0; j < (currentGeneration.size() / 2); j++) {
+                Chromosome parentA = currentGeneration.get(i);
+                Chromosome parentB = currentGeneration.get(currentGeneration.size()/2+i);
+                Chromosome child = parentA.crossover(parentA, parentB);
+                nextGeneration.add(child);
+            }
+
+            //step 4 mutate 10% and add to next gen pool
+            for (int j = 0; j < nextGeneration.size(); j++)
+                nextGeneration.get(i).mutate();
+
+            //sort next gen according to fitness
+            nextGeneration = (ArrayList<Chromosome>) sortingMethod(nextGeneration);
+
+
+            currentGeneration.clear();
+            for (int j = 0; j < 0; j++) {
+                currentGeneration.add(nextGeneration.get(j));
+            }
+            nextGeneration.clear();
+            //clear current population and take top 10 of next gen to replace it as current generation
+
+        }
+    }
+}
